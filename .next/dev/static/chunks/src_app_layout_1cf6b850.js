c204ad080b7473631776bc8e99b9c0e172e94b32
Samuel Moreno Carmona <morenosamuel2007@gmail.com>
2026-06-28 18:29:07 -0500

    remover carpeta .next del rastreo de git

--- .next/dev/static/chunks/src_app_layout_1cf6b850.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_adae5086._.js",
-  "static/chunks/src_864d05c4._.js",
-  "static/chunks/src_app_globals_91e4631d.css"
-],
-    source: "dynamic"
-});
